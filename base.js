95f682da4d354acf97a83e40eeaa442d79744071
Richard Bronosky <Richard@Bronosky.com>
2013-08-14 00:58:20 -0400

    cleanup

--- base.js
+++ base.js
@@ -1,56 +1,37 @@
 $(function(){
-    $(window).load(function(event){
-        el = $('.header').first()
-        $("<div style='height:"+(el.next().offset().top-parseInt($('p',el.next()).first().css('margin-top')))+"px'>foo bar</div>").insertAfter(el)
-        el.first().addClass('fixed');
-    });
-
+    var el = $('.header').first();
+    var top_margin = parseInt($('p',el.next()).first().css('margin-top'))
     var lastScrollTop = 0;
+    $("<div style='height:"+(el.next().offset().top-top_margin)+"px'>foo bar</div>").insertAfter(el)
+    el.first().addClass('fixed');
+
     $(window).scroll(function(event){
         var currentScrollTop = $(this).scrollTop();
-        if (currentScrollTop > lastScrollTop){
-            console.log('scrolling down');
-            $('.header').each(function(){
-                if($(this).hasClass('fixed'))
-                {
+        $('.header').each(function(){
+            if($(this).hasClass('fixed')){
+                if (currentScrollTop > lastScrollTop){
+                    console.log('scrolling down');
                     var _next_header = $(this).nextUntil('.header').next('.header');
-                    console.log("_next_header", _next_header);
-                    //debugger;
-                    if($(_next_header).length > 0)
-                    {
-                        console.log($(_next_header).offset().top, "<", $('body').scrollTop())
-                        if($(_next_header).offset().top < $('body').scrollTop())
-                        {
+                    if($(_next_header).length > 0){
+                        if($('body').scrollTop() > $(_next_header).offset().top){
                             // Bottom of header hit top of next header
                             $(this).removeClass('fixed').addClass('relative');
                             $(_next_header).removeClass('relative').addClass('fixed');
                         }
                     }
-                }
-            }); 
-        } 
-        else 
-        {
-           console.log('scrolling up');
-            // Scrolling up
-            $('.header').each(function(){
-                if($(this).hasClass('fixed'))
-                { 
+                } else {
+                    console.log('scrolling up');
                     var _prev_header = $(this).prevUntil('.header').prev('.header');
-                    if($(_prev_header ).length > 0)
-                    {
-                        //if($(this).offset().top <= ($('#' + $(_prev_header).attr('id') + '_content').offset().top + $(this).height()))
-                        console.log($(_prev_header).offset().top, "<", $('body').scrollTop())
-                        if($('.fixed').next().offset().top >= $('body').scrollTop()+(2*$('.fixed').height()))
-                        {
+                    if($(_prev_header ).length > 0){
+                        if($('body').scrollTop() < $('.fixed').next().offset().top-2*($('.fixed').height()-2*top_margin+1)){
                             // Top of header hit bottom of previous content box
                             $(this).removeClass('fixed').addClass('relative');
                             $(_prev_header).removeClass('relative').addClass('fixed');
                         }
                     }
                 }
-            }); 
-        }
+            }
+        }); 
         lastScrollTop = currentScrollTop;
     });
 });
