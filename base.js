15d6f61a96636f9a26b24fc58dbcb365f53f589b
Richard Bronosky <Richard@Bronosky.com>
2013-08-14 23:58:52 -0400

    simplify

--- base.js
+++ base.js
@@ -1,9 +1,10 @@
+// From: http://jsfiddle.net/AlienWebguy/mvtP7/1/
 $(function(){
-    var el = $('.header').first();
-    var top_margin = parseInt($('p',el.next()).first().css('margin-top'))
     var lastScrollTop = 0;
-    $("<div style='height:"+(el.next().offset().top-top_margin)+"px'>foo bar</div>").insertAfter(el)
-    el.first().addClass('fixed');
+    // When the first header is given a fixed position, the inline content that follows with shift up.
+    el = $('.header').first();
+    // To over come this: clone it, make it invisible, remove id tags from it and its children, attach it before the original
+    el.before(el.clone(false).css({'visibility':'hidden'}).removeAttr("id").find("*").removeAttr("id").end()).addClass('fixed');
 
     $(window).scroll(function(event){
         var currentScrollTop = $(this).scrollTop();
@@ -14,19 +15,19 @@ $(function(){
                     var _next_header = $(this).nextUntil('.header').next('.header');
                     if($(_next_header).length > 0){
                         if($('body').scrollTop() > $(_next_header).offset().top){
-                            // Bottom of header hit top of next header
-                            $(this).removeClass('fixed').addClass('relative');
-                            $(_next_header).removeClass('relative').addClass('fixed');
+                            console.log("Bottom of header hit top of next header")
+                            $(this).removeClass('fixed');
+                            $(_next_header).addClass('fixed');
                         }
                     }
                 } else {
                     console.log('scrolling up');
                     var _prev_header = $(this).prevUntil('.header').prev('.header');
                     if($(_prev_header ).length > 0){
-                        if($('body').scrollTop() < $('.fixed').next().offset().top-2*($('.fixed').height()-2*top_margin+1)){
-                            // Top of header hit bottom of previous content box
-                            $(this).removeClass('fixed').addClass('relative');
-                            $(_prev_header).removeClass('relative').addClass('fixed');
+                        if($('body').scrollTop() < $('.fixed').next().offset().top-$('.fixed').height()){
+                            console.log("Top of header hit bottom of previous content box")
+                            $(this).removeClass('fixed');
+                            $(_prev_header).addClass('fixed');
                         }
                     }
                 }
